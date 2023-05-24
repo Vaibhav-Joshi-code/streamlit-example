8d3c54363cb9d69dd345235b09d5e0910821a377
Vaibhav Joshi <85349542+Vaibhav-Joshi-code@users.noreply.github.com>
2023-05-24 08:28:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,32 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+from pytube import YouTube
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.write('Welcome to YouTube video downloader!!')
+def main():
+    path = st.text_input('Enter URL of the video: ')
+    options = st.selectbox("Select the type of download",
+                           ('audio only','high resolution'))
+    
+    
+    if st.button('download'):
+        video_object = YouTube(path)
+        st.write('Video title: ',str(video_object.title))
+        if options == 'audio only':
+            video_object.streams.get_audio_only().download()
+        elif options == 'high resolution':
+            video_object.streams.get_highest_resolution().download()
+    if st.button("view"):
+        st.video(path)
+    st.write('Created by: Vaibhav Joshi')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if __name__ == '__main__':
+    main()
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+
