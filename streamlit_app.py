c9f64b5a6163e8c2baf4ee84def31a5ef98dafac
Vaibhav Joshi <85349542+Vaibhav-Joshi-code@users.noreply.github.com>
2023-05-24 08:43:38 +0530

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,37 @@
 import streamlit as st
-
+import os
 from pytube import YouTube
 
 st.write('Welcome to YouTube video downloader!!')
+
 def main():
     path = st.text_input('Enter URL of the video: ')
     options = st.selectbox("Select the type of download",
-                           ('audio only','high resolution'))
+                           ('audio only', 'high resolution'))
     
-    
-    if st.button('download'):
+    if st.button('Download'):
         video_object = YouTube(path)
-        st.write('Video title: ',str(video_object.title))
+        st.write('Video title:', str(video_object.title))
+        
         if options == 'audio only':
-            video_object.streams.get_audio_only().download()
+            stream = video_object.streams.get_audio_only()
         elif options == 'high resolution':
-            video_object.streams.get_highest_resolution().download()
-    if st.button("view"):
-        st.video(path)
+            stream = video_object.streams.get_highest_resolution()
+        
+        if stream:
+            filename = stream.download()
+            st.write('Download complete:', filename)
+            st.success('Video downloaded successfully!')
+        else:
+            st.error('Failed to retrieve the selected stream.')
+    
+    if st.button("View"):
+        if os.path.exists(filename):
+            st.video(filename)
+        else:
+            st.error('Video file does not exist.')
+    
     st.write('Created by: Vaibhav Joshi')
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
